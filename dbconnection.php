29819571ef8ab99691e700f80d54ed49da7a99b6
Josh <jcttierra@gmail.com>
2017-05-30 17:27:07 -0400

    Connection test 3

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
-	define("DB_SERVER", "MYSQLCONNSTR_localdb");
-	define("DB_USERNAME", "root");
-	define("DB_PASSWORD", "");
+	define("DB_SERVER", "localhost");
+	define("DB_USERNAME", "trinchero_user");
+	define("DB_PASSWORD", "Trincher0");
 	define("DB_DATABASE", "trincherodb");
 	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
