469b3dd8616dbf1c32c12713f56a888c4a6170ec
Josh <jcttierra@gmail.com>
2017-05-31 04:52:09 -0400

    Changed incorrect login

--- index.php
+++ index.php
@@ -24,8 +24,7 @@
          
          header("location: mainmenu.php");
       }else {
-		  header("location: mainmenu.php");
-         $error = "Your Email or Password is invalid";
+		 echo "Wrong password";
       }
    }
 ?>
