c710a33b96261d13d9f4f21be6fa6923582fde3d
Josh <jcttierra@gmail.com>
2017-05-31 00:49:07 -0400

    test

--- dbconnection.php
+++ dbconnection.php
@@ -32,7 +32,7 @@ $db = $dbLink = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 if ($db)
 {
-    echo "Successfully connected to the database.\n" + $connectstr_dbname;
+    echo "Successfully connected to the database.\n".$connectstr_dbname;
 }
 else
 {
