74b71dcf47edfab90dac2b3b33dafb51d1ae5711
Josh <jcttierra@gmail.com>
2017-05-31 01:01:42 -0400

    Test connection
    *Fingers crossed*

--- dbconnection.php
+++ dbconnection.php
@@ -1,41 +1,7 @@
 <?php
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-
-foreach ($_SERVER as $key => $value)
-{
-    if (strpos($key, "MYSQLCONNSTR_") !== 0)
-    {
-        continue;
-    }
-
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
-define('DB_NAME', $connectstr_dbname);
-define('DB_USER', $connectstr_dbusername);
-define('DB_PASSWORD', $connectstr_dbpassword);
-define('DB_HOST', $connectstr_dbhost);
-
-// Custom testing
-echo $connectstr_dbname."<br>";
-echo $connectstr_dbusername."<br>";
-echo $connectstr_dbpassword."<br>";
-echo $connectstr_dbhost."<br>";
-
-$db = $dbLink = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-if ($db)
-{
-    echo "Successfully connected to the database.\n";
-}
-else
-{
-    echo "Failed to connect to the database.\n";
-}
+	define("DB_SERVER", "localhost");
+	define("DB_USERNAME", "root");
+	define("DB_PASSWORD", "");
+	define("DB_DATABASE", "trincherodb");
+	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
