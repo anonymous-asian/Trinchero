b4f0e34addb29181d1afc51702367f84f0a77f9c
Josh <jcttierra@gmail.com>
2017-05-31 01:19:35 -0400

    test connection
    *fingers crossed*

--- dbconnection.php
+++ dbconnection.php
@@ -1,13 +1,17 @@
 <?php
-	define("DB_SERVER", "localhost");
-	define("DB_USERNAME", "root");
-	define("DB_PASSWORD", "");
-	define("DB_DATABASE", "trincherodb");
-	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-	
-	if($db){
-		
-	} else {
-		echo "Failed! :(";
-	}
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
 ?>
