f1d69e1d756258f1df7153bd924d207bbd745510
Josh <jcttierra@gmail.com>
2017-05-31 00:23:46 -0400

    Fix connection

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 	define("DB_SERVER", "127.0.0.1:54631");
-	define("DB_USERNAME", "azure");
-	define("DB_PASSWORD", "6#vWHD_$");
+	define("DB_USERNAME", "root");
+	define("DB_PASSWORD", "");
 	define("DB_DATABASE", "trincherodb");
 	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
