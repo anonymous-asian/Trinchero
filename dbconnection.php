7bdb64a4ff519b6f3228b37ae89c3b27f16af596
Josh <jcttierra@gmail.com>
2017-05-31 01:03:04 -0400

    test

--- dbconnection.php
+++ dbconnection.php
@@ -4,4 +4,10 @@
 	define("DB_PASSWORD", "");
 	define("DB_DATABASE", "trincherodb");
 	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+	
+	if($db){
+		
+	} else {
+		echo "Failed! :(";
+	}
 ?>
