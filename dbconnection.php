27d1e652982e9e3378f89f0bb508bda1cb9d580f
Josh <jcttierra@gmail.com>
2017-05-31 00:29:49 -0400

    connect

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,43 @@
 <?php
-	define("DB_SERVER", "127.0.0.1:54631");
-	define("DB_USERNAME", "azure");
-	define("DB_PASSWORD", "6#vWHD_$");
-	define("DB_DATABASE", "localdb");
-	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value)
+{
+    if (strpos($key, "MYSQLCONNSTR_") !== 0)
+    {
+        continue;
+    }
+
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+
+    echo $value;
+}
+
+define('DB_NAME', $connectstr_dbname);
+define('DB_USER', $connectstr_dbusername);
+define('DB_PASSWORD', $connectstr_dbpassword);
+define('DB_HOST', $connectstr_dbhost);
+
+// Custom testing
+echo $connectstr_dbname."\n";
+echo $connectstr_dbusername."\n";
+echo $connectstr_dbpassword."\n";
+echo $connectstr_dbhost."\n";
+
+$db = $dbLink = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if ($db)
+{
+    echo "Successfully connected to the database.\n";
+}
+else
+{
+    echo "Failed to connect to the database.\n";
+}
 ?>
